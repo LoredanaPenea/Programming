ea298e68742bc17fa62414672dd73d7361fea258
Loredana Penea <Nicoleta-Loredana.Penea@endava.com>
2024-11-28 15:46:14 +0200

    Homework1 - Test

---
+++ TrainingPrograming/Session2/Cursant.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TrainingPrograming.Session2
+{
+    public class Cursant
+    {
+        string FirstName;
+        string LastName;
+        int age;
+        bool isActive;
+        char gendre;
+        float grade;
+        double height;
+
+        [Test]
+        public void RunCursant()
+        {
+            FirstName = "Loredana";
+            LastName = "Penea";
+            age = 36;
+            isActive = true;
+            gendre = 'F';
+            grade = 8.75f;
+            height = 1.65;
+            Console.WriteLine("Cursant details:");
+            Console.WriteLine("Name is: " + FirstName + " " + LastName);
+            Console.Write("Age is: "+ age + "\n");
+            Console.WriteLine("Height: " + height);
+            Console.Write("You will be " + (age+20) + " years old in 20 years!");
+        }
+
+    }
+}
