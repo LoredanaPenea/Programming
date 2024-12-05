548a77d1949d871800a98deddce78a530039a419
Loredana Penea <Nicoleta-Loredana.Penea@endava.com>
2024-12-05 09:54:46 +0200

    homework2 - create static class person

---
+++ TrainingPrograming/Session3/Method1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TrainingPrograming.Session3
+{
+    public class Method1
+    {
+        [Test]
+        public void MethodTest ()
+        {
+            Sum2Numbers();
+            Sum2Numbers(10, 20);
+            int sum = GetResult(15, 33, 47);
+            Console.WriteLine($"The result is {sum}");
+        }
+      
+        public void Sum2Numbers()
+        {
+            int firstNumber = 30;
+            int secondNumber = 123;
+            int sum = firstNumber + secondNumber;   
+            Console.WriteLine($"Sum of 2 numbers is: {sum}");
+        }
+
+        public void Sum2Numbers (int firstNumber, int secondNumber)
+        {
+            int sum = firstNumber + secondNumber;
+            Console.WriteLine($"Sum of 2 numbers is: {sum}");
+        }
+
+        public int GetResult(int firstNumber, int secondNumber, int thirdNumber)
+        {
+            int sum = firstNumber + secondNumber + thirdNumber;
+            return sum;
+        }
+
+    }
+}
---
+++ TrainingPrograming/Session3/Person.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TrainingPrograming.Session3
+{
+    public static class Person
+     {
+        public static string FirstName = "Loredana";
+        public static string LastName = "Penea";
+        public static int Age = 36;
+        public static string Gender = "Female";
+
+        [Test]
+        public static void MethodHomework()
+        {
+            int xAge = Person.AgeInXYears(15);
+            Person.DisplayPersonDetails();
+            Console.WriteLine($"You will be {xAge} years old in 15 years!");
+
+        }
+
+        public static int AgeInXYears(int x)
+        {
+             return x + Age;
+        }
+
+        public static void DisplayPersonDetails()
+        {
+            Console.WriteLine($"Person details are as follows: \n" +
+                    $"- First Name: {FirstName} \n" +
+                    $"- Last Name: {LastName} \n" +
+                    $"- Gender: {Gender}");
+        }
+    }
+}
+
