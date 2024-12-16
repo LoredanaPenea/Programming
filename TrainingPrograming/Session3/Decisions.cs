25e9b9ecc94c76c2f7c2886917094deed1b0bca7
Loredana Penea <Nicoleta-Loredana.Penea@endava.com>
2024-12-16 16:32:13 +0200

    homework - if-else

---
+++ TrainingPrograming/Session3/Decisions.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TrainingPrograming.Session3
+{
+    public class Decisions
+    {
+        [Test]
+        
+        public void DecisionsTest()
+        {
+            IsOld();
+            ShowGender();
+        }
+        public void IsOld()
+        {
+            if (Person.Age >= 100) {
+                Console.WriteLine("You are ancient!");
+            } else if (Person.Age >= 50) {
+                Console.WriteLine("You are old!");
+            } else Console.WriteLine("You are still young!");
+        }
+        
+        public void ShowGender()
+        {
+            if (Person.Gender.Equals("Male"))
+            {
+                Console.WriteLine("You are a male!");
+            } else if (Person.Gender.Equals("Female"))
+                Console.WriteLine("You are a female!");
+            else Console.WriteLine("You haven't specified you gender!");
+        }
+    }
+}
---
+++ TrainingPrograming/Session4/DecisionMaking.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TrainingPrograming.Session4
+{
+    public class DecisionMaking
+    {
+    [Test]
+
+    public void TestMethod()
+     {
+            VerifyNumber(7,11);
+            Compare(8);
+            VerifyCar("Opel");
+     }
+
+    public void VerifyNumber (int a, int b)
+        {
+            if (a < b)
+            {
+                Console.WriteLine($"Number {a} is smaller than number {b}");
+            }
+            else
+            {
+                Console.WriteLine($"Number {a} is higher than number {b}");
+            }
+        }
+
+    public void Compare (int x)
+        {
+            if (x < 5)
+            {
+                Console.WriteLine($"Number {x} is smaller than 5");
+            }
+            else if (x>5) 
+            {
+                Console.WriteLine($"Number {x} is bigger than 5");
+            }
+            else
+            {
+                Console.WriteLine($"Number {x} equals 5");
+            }
+        }
+
+    public void VerifyCar(string car)
+        {
+            switch (car)
+            {
+                case "Audi":
+                    Console.WriteLine("Audi is available");
+                    break;
+                case "Mercesdes":
+                    Console.WriteLine("Mercedes is available");
+                    break;
+                case "BMW":
+                    Console.WriteLine("BMW is available");
+                    break;
+                default:
+                    Console.WriteLine("None");
+                    break;
+            }
+        }
+
+    }
+}
