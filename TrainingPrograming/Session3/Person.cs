b0b20904d8020a3aed574fe6bf326e81a0932d12
Loredana Penea <Nicoleta-Loredana.Penea@endava.com>
2025-01-20 11:09:09 +0200

    Add Skills to Person

--- TrainingPrograming/Session3/Person.cs
+++ TrainingPrograming/Session3/Person.cs
@@ -12,6 +12,7 @@ namespace TrainingPrograming.Session3
         public static string LastName = "Penea";
         public static int Age = 36;
         public static string Gender = "Bi";
+        public static List<string> Skills = new List<string>();
 
         [Test]
         public static void MethodHomework()
@@ -20,19 +21,53 @@ namespace TrainingPrograming.Session3
             Person.DisplayPersonDetails();
             Console.WriteLine($"You will be {xAge} years old in 15 years!");
 
+            Person.AddSkills("Software");
+            Person.AddSkills("Testing");
+            Person.AddSkills("Gardening");
+            Person.AddSkills("Ninja");
+            Person.AddSkills("Dancing");
+            Person.AddSkills("Driving");
+            Person.AddSkills("CIA");
+            Person.AddSkills("Spanish Language");
+            Person.AddSkills("Baking");
+
+            Person.DisplayPersonSkills();
+
         }
 
         public static int AgeInXYears(int x)
         {
              return x + Age;
         }
-
+       
         public static void DisplayPersonDetails()
         {
             Console.WriteLine($"Person details are as follows: \n" +
                     $"- First Name: {FirstName} \n" +
                     $"- Last Name: {LastName} \n" +
                     $"- Gender: {Gender}");
+        }
+
+        public static void AddSkills(string skill)
+        {
+            Console.WriteLine($"Added  a new skill: {skill}");
+            Skills.Add(skill);
+        }
+
+        public static void DisplayPersonSkills()
+        {
+            Console.WriteLine("The person has the following skills:");
+
+            foreach (string skill in Skills)
+            {
+                if (!skill.Equals("Ninja") && (!skill.Equals("CIA")))
+                    Console.Write(skill + "; ");
+                else if (skill.Equals("CIA"))
+                {
+                    Console.WriteLine("\n >>>>> Classified information, no further skilss are displayed! <<<<<<");
+                    break;
+                }
+            }
         }
     }
 }
---
+++ TrainingPrograming/Session_16_Jan/IterationStatements.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TrainingPrograming.Session_16_Jan
+{
+    public class IterationStatements
+    {
+        [Test]
+
+        public void TestExample()
+        {
+            /*for (int i = 1; i <= 7; i++)
+            {
+                Console.WriteLine(i);
+            }
+
+            int j = 1;
+            for(; j<=8; j++)
+            {
+                Console.WriteLine(j);
+            }*/
+
+            string[] coleagueName = new string[3];
+            coleagueName[0] = "A";
+            coleagueName[1] = "B";
+            coleagueName[2] = "C";
+
+            for (int i = 0; i < coleagueName.Length; i++)
+            {
+                Console.WriteLine(coleagueName[i]);
+            }
+
+            List<string> Names = new List<string>
+            {
+                "Lore",
+                "Mihai",
+                "Andra"
+            };
+
+            foreach (string name in Names)
+            {
+                Console.WriteLine(name);
+            }
+        }
+    }
+}
