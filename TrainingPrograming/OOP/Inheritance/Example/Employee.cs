7aa2d294688b4de9cbafdc61a5b79751de83f069
Loredana Penea <Nicoleta-Loredana.Penea@endava.com>
2025-05-14 10:19:40 +0300

    move files to Example folder

--- TrainingPrograming/OOP/Inheritance/Example/Employee.cs
+++ TrainingPrograming/OOP/Inheritance/Example/Employee.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using TrainingPrograming.OOP.Inheritance.Example;
 
 namespace TrainingPrograming.OOP.Inheritance
 {
